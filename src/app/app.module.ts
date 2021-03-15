d03463c1485c5e178f330f7d7b0683c3d766060d
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2021-03-15 13:50:48 -0300

    fix: Remove my-counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-
-import {StoreComponent} from './store/store.component';
-import { counterReducer} from "./store/counter.reducer";
-import { MyCounterComponent } from './store/my-counter/my-counter.component';
-import {StoreModule} from '@ngrx/store';
+import {AppComponent} from './app.component';
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StoreComponent,
-    MyCounterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    // StoreModule.forRoot({ count: counterReducer })
-    StoreModule.forRoot({count: counterReducer}),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
