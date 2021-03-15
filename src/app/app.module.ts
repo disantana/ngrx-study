c06f0d542d8086f5b95fc7896a3e7d38685c0214
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2021-03-15 14:35:11 -0300

    feat: Atualiza app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {environment} from "../environments/environment";
+import {StoreModule} from "@ngrx/store";
+import {booksReducer} from "./state/books.reducer";
+import {collectionReducer} from "./state/collection.reducer";
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookCollectionComponent
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
     }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
