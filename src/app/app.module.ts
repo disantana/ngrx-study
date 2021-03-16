9c1566b54e81076a51eb87e16063cdc7c5324f1c
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2021-03-16 10:28:31 -0300

    feat: Adiciona o componente movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {environment} from "../environments/environment";
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
